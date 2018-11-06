f7fd68c7350f043eff7a3990011362b065257c73
Phelipe Alves de Souza <sougyn@gmail.com>
2018-11-06 14:49:22 -0200

    Adding contiv sdk

---
+++ README.md
@@ -0,0 +1,7 @@
+# contiv-python-sdk
+
+[![Build Status](https://img.shields.io/travis/sw4iot/contiv-python-sdk.svg)](https://travis-ci.org/sw4iot/contiv-python-sdk)
+[![PyPI](https://img.shields.io/pypi/v/contiv-python-sdk.svg)](https://pypi.org/project/contiv-python-sdk/)
+[![Updates](https://pyup.io/repos/github/sw4iot/contiv-python-sdk/shield.svg)](https://pyup.io/repos/github/sw4iot/contiv-python-sdk/)
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/contiv-python-sdk.svg)
+[![Hex.pm](https://img.shields.io/hexpm/l/plug.svg)](https://www.apache.org/licenses/LICENSE-2.0)
---
+++ setup.py
@@ -0,0 +1,48 @@
+import setuptools
+import re
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+def load_reqs(filename):
+    with open(filename) as reqs_file:
+        return [
+            re.sub('==', '>=', line) for line in reqs_file.readlines()
+            if not re.match('\s*#', line)
+        ]
+
+
+requirements = load_reqs('requirements.txt')
+
+setuptools.setup(
+    name="contiv-python-sdk",
+    version="0.0.1",
+    author="SOFTWAY4IoT",
+    author_email="softway4iot@gmail.com",
+    description="contiv-python-sdk of SOFTWAY4IoT",
+    license="Apache Software License",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sw4iot/contiv-python-sdk",
+    install_requires=requirements,
+    packages=[
+        'contiv_sdk'
+    ],
+    package_dir={
+        'contiv_sdk': 'contiv_sdk'
+    },
+    keywords='contiv-sdk',
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ]
+)
