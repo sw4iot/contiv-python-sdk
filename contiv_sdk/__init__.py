3013685124c151a7b2edc32f942bd9cdee29acc9
n3wt0nSAN <josephnewton001@gmail.com>
2018-11-09 18:08:54 -0200

    sdk to create policy, rules and endpoint groups

--- contiv_sdk/__init__.py
+++ contiv_sdk/__init__.py
@@ -48,6 +48,18 @@ class ContivSdk:
         return requests.post('{}/{}/'.format(self.base_url, url), json=data, headers=self.__headers(token),
                              verify=False)
 
+    def __put_request(self, url, data, token=True):
+        """
+        Put request
+
+        :param url:
+        :param data:
+        :param token:
+        :return:
+        """
+        return requests.put('{}/{}/'.format(self.base_url, url), json=data,
+                            headers=self.__headers(token), verify=False)
+
     def __delete_request(self, url):
         """
         Delete request
@@ -123,4 +135,159 @@ class ContivSdk:
         :return:
         """
         r = self.__delete_request('networks/{}:{}-net'.format(tenant_name, tenant_name))
+        return True if r.status_code == requests.codes.ok else None
+
+    def post_policy(self, tenant_name, policy_name):
+        """
+        Create network policy
+
+        :param tenant_name:
+        :param policy_name:
+        :return:
+        """
+
+        key = '{}:{}'.format(tenant_name, policy_name)
+        r = self.__post_request(
+            'policys/{}:{}'.format(tenant_name, policy_name),
+            data={
+                'key': key,
+                'policyName': policy_name,
+                'tenantName': tenant_name
+            })
+
+        return json.loads(r.content) \
+            if r.status_code == requests.codes.ok else None
+
+    def delete_policy(self, policy_key):
+        """
+        Delete a network policy with all rules
+
+        :param policy_key: "%s:%s" (tenant_name, policy_name)
+        :return:
+        """
+
+        r = self.__delete_request('policys/{}'.format(policy_key))
+        return True if r.status_code == requests.codes.ok else None
+
+    def post_rule(self, tenant_name, policy_name, rule_id, rule_priority,
+                  rule_action, rule_to_epg, rule_to_net, rule_to_addr,
+                  rule_protocol, rule_direction, rule_port=0):
+        """
+        Create network rule to compose a policy
+
+        :param policy_name:
+        :param tenant_name:
+        :param rule_action: "allow or deny"
+        :param rule_direction: "out or in"
+        :param rule_port: int
+        :param rule_priority: int
+        :param rule_protocol: "tcp, udp or icmp"
+        :param rule_id:
+        :param rule_to_epg: "toEndpontGroup"
+        :param rule_to_addr: "toIpAddress"
+        :param rule_to_net: "toNetwork"
+        :return:
+        """
+
+        key = '{}:{}:{}'.format(tenant_name, policy_name, rule_id)
+        r = self.__post_request(
+            'rules/{}'.format(key),
+            data={
+                'tenantName': tenant_name,
+                'policyName': policy_name,
+                'key': key,
+                'ruleId': rule_id,
+                'priority': rule_priority,
+                'action': rule_action,
+                'toEndpointGroup': rule_to_epg,
+                'toNetwork': rule_to_net,
+                'toIpAddress': rule_to_addr,
+                'protocol': rule_protocol,
+                'port': rule_port,
+                'direction': rule_direction
+            })
+
+        return json.loads(r.content) \
+            if r.status_code == requests.codes.ok else None
+
+    def delete_rule(self, rule_key):
+        """
+        Delete a network rule
+
+        :param rule_key
+        :return:
+        """
+
+        r = self.__delete_request('rules/{}'.format(rule_key))
+        return True if r.status_code == requests.codes.ok else None
+
+    def post_endpoint_group(self, tenant_name, tenant_net, group_name,
+                            group_policies):
+        """
+        Create endpoint group with policys to apply
+
+        :param group_name:
+        :param tenant_name:
+        :param tenant_net:
+        :param group_policies: []
+        :return:
+        """
+
+        key = '{}:{}'.format(tenant_name, group_name)
+        r = self.__post_request(
+            'endpointGroups/{}'.format(key),
+            data={
+                'groupName': group_name,
+                'networkName': tenant_net,
+                'policies': group_policies,
+                'netProfile': '',
+                'extContractsGrps': [],
+                'tenantName': tenant_name,
+                'key': key
+            })
+
+        return json.loads(r.content) \
+            if r.status_code == requests.codes.ok else None
+
+    def put_endpoint_group(self, tenant_name, tenant_net, group_name,
+                           group_policies):
+        """
+        Add or delete one policy or more to a endpoint group
+        :param group_name:
+        :param tenant_name:
+        :param tenant_net:
+        :param group_policies: []
+        :return:
+        """
+
+        key = '{}:{}'.format(tenant_name, group_name)
+        r = self.__put_request(
+            'endpointGroups/{}'.format(key),
+            data={
+                'groupName': group_name,
+                'networkName': tenant_net,
+                'policies': group_policies,
+                'netProfile': '',
+                'networkName': tenant_net,
+                'links-sets': {},
+                'links': {'AppProfile': {}, 'NetProfile': {},
+                          'Network': {'type': 'network',
+                                      'key': '{}:{}'.format(tenant_name,
+                                                            tenant_net)},
+                          'Tenant': {'type': 'tenant', 'key': tenant_name}},
+                'tenantName': tenant_name,
+                'key': key
+            })
+
+        return True if r.status_code == requests.codes.ok else None
+
+    def delete_endpoint_group(self, group_key):
+        """
+        Delete a endpoint group with all your policies
+
+        :param group_key
+        :return:
+        """
+
+        r = self.__delete_request('endpointGroups/{}'.format(group_key))
         return True if r.status_code == requests.codes.ok else None
